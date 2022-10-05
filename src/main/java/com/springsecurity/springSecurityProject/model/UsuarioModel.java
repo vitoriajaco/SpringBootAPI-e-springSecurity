32c90140ee69b5e95fac456759abfa9e986731c4
vitoriajaco <vitoriajaco@gmail.com>
2022-10-05 18:14:38 -0300

    Alterando int por String

--- src/main/java/com/springsecurity/springSecurityProject/model/UsuarioModel.java
+++ src/main/java/com/springsecurity/springSecurityProject/model/UsuarioModel.java
@@ -1,36 +1,35 @@
 package com.springsecurity.springSecurityProject.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "usuarios")
-public class UsuarioModel {
+public class UsuarioModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String nome;
-    @Column (nullable = false, length = 2)
-    private int idade;
-    @Column(nullable = false, length = 11)
+    @Column (nullable = false)
+    private String idade;
+    @Column(nullable = false)
     private String telefone;
     @Column(nullable = true, unique = true)
     private String login;
-    @Column(nullable = true, length = 6)
+    @Column(nullable = true)
     private String senha;
 
-    public UsuarioModel(Long id, String nome, int idade, String telefone, String login, String senha) {
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.telefone = telefone;
-        this.login = login;
-        this.senha = senha;
-    }
+
+
+
+
 }
