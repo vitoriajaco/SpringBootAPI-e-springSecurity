fb715f3e1e482e2cb8a53dbbba31cea05b35b970
vitoriajaco <vitoriajaco@gmail.com>
2022-10-05 18:24:41 -0300

    comentarios

--- src/main/java/com/springsecurity/springSecurityProject/security/SecurityConfig.java
+++ src/main/java/com/springsecurity/springSecurityProject/security/SecurityConfig.java
@@ -9,8 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    //Permite que tudo que esteja dentro desse path não necessite que passe senha, no fim determina o tipo da transação que é a basic
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //            |-> desabilita o csrf |-> vai autorizar os proximos parametros
         http.csrf().disable().authorizeHttpRequests().antMatchers("/usuarios/create")
                 .permitAll().and().httpBasic();
     }
